4e4e4879fb033dedfd3bc71057cc51ee5f49e2b6
rys4590 <ronnie.yadid@gmail.com>
2025-12-17 20:51:04 +0200

    shld also be in basic

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/JoystickBindings.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.Robot;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+public class JoystickBindings {
+    public final Gamepad gamepad1;
+    public final Gamepad gamepad2;
+
+    public JoystickBindings(Gamepad gamepad1, Gamepad gamepad2){
+        this.gamepad1=gamepad1;
+        this.gamepad2=gamepad2;
+    }
+    public void mainJoystickButtons(Robot robot){
+    }
+}
