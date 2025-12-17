a1ee473c80e1db29e7e36b51877913f37137a0ec
rys4590 <ronnie.yadid@gmail.com>
2025-12-17 20:47:46 +0200

    tank

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Tank.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Tank.java
@@ -6,15 +6,31 @@ public class Tank {
     private final DcMotor leftMotor;
     private final DcMotor rightMotor;
 
-    private static final double deltaBetweenMotorsToTurn90DegTPS = 720;
-    private static final double blindaMotorLimtTPS = 3600;
+    private static final double fixedAngleRatio = 4/5;
+    private static final double tolerance = 0.05;
 
     public Tank(DcMotor rightMotor, DcMotor leftMotor){
         this.leftMotor=leftMotor;
         this.rightMotor=rightMotor;
     }
 
-    public void goToPosition(int degrees){
+    public void goToPosition(double degrees, double power){
+        double reasonablePower = 0.6*power;
+        if(!(MathUtilBlitz.isNear(0,degrees,tolerance)||MathUtilBlitz.isNear(180, reasonablePower, tolerance)&& degrees>0)){
+            rightMotor.setPower(reasonablePower*fixedAngleRatio);
+            leftMotor.setPower(reasonablePower);
+        } else if(!(MathUtilBlitz.isNear(0, degrees, tolerance) || MathUtilBlitz.isNear(180, degrees, tolerance))){
+            rightMotor.setPower(reasonablePower);
+            leftMotor.setPower(reasonablePower*fixedAngleRatio);
+        }
+        else{
+            leftMotor.setPower(reasonablePower);
+            rightMotor.setPower(reasonablePower);
+        }
+    }
 
+    public void stop(){
+        rightMotor.setPower(0);
+        leftMotor.setPower(0);
     }
 }
