50c3ae78ad0cd6c1fd6b2f731240767c75ade3dd
chrissy-wake-up <bill.b.s.sk@gmail.com>
2025-12-19 15:00:15 +0200

    idk fixing stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Tank.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Tank.java
@@ -3,7 +3,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 public class Tank {
     private final DcMotor leftMotor;
     private final DcMotor rightMotor;
-    private static final double powerPerVelocity = 5;
+    private static final double powerPerVelocity = 1;
     public Tank(DcMotor rightMotor, DcMotor leftMotor){
         this.leftMotor=leftMotor;
         this.rightMotor=rightMotor;
@@ -11,6 +11,10 @@ public class Tank {
     public void moveWithStickXY(double stickX, double stickY) {
         double left = stickY - stickX;
         double right = stickY + stickX;
+        if (Math.max(left,right)>1){
+            left/=Math.max(left,right);
+            right/=Math.max(left,right);
+        }
         rightMotor.setPower(right*powerPerVelocity);
         leftMotor.setPower(left*powerPerVelocity);
     }
