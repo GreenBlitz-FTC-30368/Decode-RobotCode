f4840820058e4f44ebbde96784d2023c55524d86
rys4590 <ronnie.yadid@gmail.com>
2026-01-05 20:07:19 +0200

    adam

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Rail.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.Robot;
+
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+public class Rail {
+    private final CRServo servoRight;
+    private final CRServo servoLeft;
+
+    public Rail(CRServo servoRight, CRServo servoLeft){
+        this.servoLeft = servoLeft;
+        this.servoRight = servoRight;
+    }
+
+    public void stop(){
+        servoRight.setPower(0);
+        servoLeft.setPower(0);
+    }
+
+    public void go(){
+        servoRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        servoLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        servoLeft.setPower(0.6);
+        servoRight.setPower(0.6);
+    }
+}
