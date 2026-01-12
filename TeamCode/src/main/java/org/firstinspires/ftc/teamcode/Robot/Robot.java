9808562702b02f278edd2650324d2e077fcd834e
rys4590 <ronnie.yadid@gmail.com>
2026-01-12 17:43:37 +0200

    achmed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Robot.java
@@ -1,5 +1,11 @@
 package org.firstinspires.ftc.teamcode.Robot;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
 public class Robot {
     Rail rail;
+
+    public Robot(){
+        this.rail = new Rail(hardwareMap.crservo.get("railRight"), hardwareMap.crservo.get("railLeft"));
+    }
 }
