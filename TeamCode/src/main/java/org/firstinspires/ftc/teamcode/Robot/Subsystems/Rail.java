d6a9f3f485d5b3b30b19a95b2f14c2335fb80072
AGRAN <agranott@gmail.com>
2026-01-13 15:56:55 +0200

    added rail mode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Subsystems/Rail.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Subsystems/Rail.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode.Robot.Subsystems;
 
+import android.app.Notification;
+
+import com.acmerobotics.roadrunner.Action;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
@@ -7,14 +10,18 @@ public class Rail {
     private final CRServo servoRight;
     private final CRServo servoLeft;
 
+    private String mode;
+
     public Rail(CRServo servoRight, CRServo servoLeft) {
         this.servoLeft = servoLeft;
         this.servoRight = servoRight;
+        mode = "none";
     }
 
     public void stop() {
         servoRight.setPower(0);
         servoLeft.setPower(0);
+        mode = "stop";
     }
 
     public void go() {
@@ -22,5 +29,10 @@ public class Rail {
         servoLeft.setDirection(DcMotorSimple.Direction.FORWARD);
         servoLeft.setPower(0.6);
         servoRight.setPower(0.6);
+        mode = "go";
+    }
+
+    public String getMode() {
+        return mode;
     }
 }
