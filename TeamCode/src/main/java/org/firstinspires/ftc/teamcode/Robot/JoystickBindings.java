b72e97796340f72a7320dc02d152326caa5266f9
AGRAN <agranott@gmail.com>
2026-02-02 19:41:50 +0200

    adam - new branch

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/JoystickBindings.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/JoystickBindings.java
@@ -36,4 +36,31 @@ public class JoystickBindings {
 
     }
 
+    public void mainJoystickButtonsMecha(RobotMecha robot) {
+        robot.getTank().moveWithStickXY(gamepad1.left_stick_x, gamepad1.right_stick_y);
+
+        if (gamepad1.right_trigger > 0.5) {
+            robot.getRail().go();
+        } else {
+            robot.getRail().stop();
+        }
+
+        if (gamepad1.left_trigger>0.1) {
+            robot.getFlywheel().shoot();
+        }
+
+        if(gamepad1.left_bumper)
+            robot.getFlywheel().stop();
+
+        if (gamepad1.back) {
+            robot.getFlywheel().stop();
+        }
+        if(gamepad1.psWasPressed()){
+            robot.getFlywheel().stop();
+            robot.getRail().stop();
+            robot.getTank().stop();
+        }
+
+    }
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/OpMode/TeleopMecha.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/OpMode/TeleopMecha.java
@@ -1,31 +1,28 @@
 package org.firstinspires.ftc.teamcode.Robot.OpMode;
 
-public class TeleopMecha {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.Robot.JoystickBindings;
+import org.firstinspires.ftc.teamcode.Robot.Robot;
+import org.firstinspires.ftc.teamcode.Robot.RobotMecha;
 
+@TeleOp(name="teleopTank")
+public class TeleopMecha extends OpMode {
 
+    private RobotMecha robot;
+    private JoystickBindings joystickBindings;
 
+    @Override
+    public void init() {
+        robot = new RobotMecha(hardwareMap);
+        joystickBindings = new JoystickBindings(gamepad1);
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    @Override
+    public void loop() {
+        joystickBindings.mainJoystickButtonsMecha(robot);
+        telemetry.addData("RailMode", robot.getRail().getMode());
+    }
 
 }
