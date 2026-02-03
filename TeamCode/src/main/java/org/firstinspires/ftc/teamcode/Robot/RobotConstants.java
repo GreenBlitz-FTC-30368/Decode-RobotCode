143fac8790257fd0a901da9f71383db8b3a26f52
chrissy-wake-up <bill.b.s.sk@gmail.com>
2026-02-03 17:30:11 +0200

    yuval maybe approved this, he didnt know what he was approving

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/RobotConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/RobotConstants.java
@@ -4,9 +4,11 @@ public class RobotConstants {
     public static final int wheelAccelerationTimeMS = 1000;
     public static final int timeToShootAnArtifactMS = 500;
     public static final int timeBetweenArtifactShootings = 1000;
-    public static final int tankRPM = 117;
-    public static final double tankWheelRadiusCm = 9.6;
-    public static final double tankMaxVelocityCmPerMinute = tankRPM * tankWheelRadiusCm * Math.PI * 2;
+    public static final int RPM = 117;
+    public static final double mecanumWheelRadiusCm = 5;
+    public static final double mecanumVerticalHorizontalVelocityCmPerMinute = RPM*mecanumWheelRadiusCm*Math.PI*2/Math.sqrt(2);
+//    public static final double tankWheelRadiusCm = 9.6;
+//    public static final double tankMaxVelocityCmPerMinute = RPM * tankWheelRadiusCm * Math.PI * 2;
     public static final double distanceToShootCm = 50;
     public static final double tileSizeCm = 60.96;
 }
