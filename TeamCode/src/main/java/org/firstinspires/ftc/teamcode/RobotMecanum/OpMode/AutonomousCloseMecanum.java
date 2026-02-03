ba022f6f328a6eaa80802eacf095e020c3963444
chrissy-wake-up <bill.b.s.sk@gmail.com>
2026-02-03 18:03:38 +0200

    idk dror forced me

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotMecanum/OpMode/AutonomousCloseMecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotMecanum/OpMode/AutonomousCloseMecanum.java
@@ -3,13 +3,13 @@ package org.firstinspires.ftc.teamcode.RobotMecanum.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import org.firstinspires.ftc.teamcode.RobotTank.RobotConstants;
-import org.firstinspires.ftc.teamcode.RobotTank.RobotMecanum;
+import org.firstinspires.ftc.teamcode.RobotMecanum.RobotConstants;
+import org.firstinspires.ftc.teamcode.RobotMecanum.RobotMecanum;
 
 @Autonomous(name = "close auto mecanum")
 public class AutonomousCloseMecanum extends LinearOpMode {
     private RobotMecanum robot;
-
+    private static int moveTimeToShootPositionMS=500;
     private void shootThreeArtifacts() {
         for (int i = 0; i < 3; i++) {
             robot.getRail().go();
