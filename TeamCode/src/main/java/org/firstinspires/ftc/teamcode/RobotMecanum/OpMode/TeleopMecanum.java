ceaec54556322dd38ef8d169bed2428eaaff9c5c
chrissy-wake-up <bill.b.s.sk@gmail.com>
2026-02-03 17:57:48 +0200

    using ronnie's and adam's teleopMecanum

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotMecanum/OpMode/TeleopMecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotMecanum/OpMode/TeleopMecanum.java
@@ -1,6 +1,27 @@
 package org.firstinspires.ftc.teamcode.RobotMecanum.OpMode;
 
-public class TeleopMecanum {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.RobotMecanum.JoystickBindings;
+import org.firstinspires.ftc.teamcode.RobotMecanum.RobotMecanum;
+
+@TeleOp(name="teleopMecanum")
+public class TeleopMecanum extends OpMode {
+
+    private RobotMecanum robot;
+    private JoystickBindings joystickBindings;
+
+    @Override
+    public void init() {
+        robot = new RobotMecanum(hardwareMap);
+        joystickBindings = new JoystickBindings(gamepad1);
+    }
+
+    @Override
+    public void loop() {
+        joystickBindings.mainJoystickButtons(robot);
+        telemetry.addData("RailMode", robot.getRail().getMode());
+    }
 
 }
